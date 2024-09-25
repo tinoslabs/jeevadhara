90ef17b0f3e644eb41a8483458b689ac5c3ac163
tinoslabs <59659322+tinoslabs@users.noreply.github.com>
2024-09-25 15:38:48 +0530

    Update settings.py

--- jeevadharapro/settings.py
+++ jeevadharapro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v5=rn=2*o5$o&@_v5meu3g_u&a$1tf=#%v$@+qn6qr#b6_-sdn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jeevadhara.onrender.com']
+ALLOWED_HOSTS = ['jeevadhara-1.onrender.com']
 
 
 # Application definition
